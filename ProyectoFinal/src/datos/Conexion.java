cac07f09d49181976383e158529aa07f8d31fbc5
JTR-14 <djtoledor@unitru.edu.pe>
2025-12-12 19:11:39 -0500

    inicio

---
+++ ProyectoFinal/src/datos/Conexion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conexion {
+    public static Connection realizarConexion() throws ClassNotFoundException, SQLException {
+        String url, user, password;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        url = "jdbc:mysql://localhost:3306/alquiler_automoviles";
+        user = "root";
+        password = "natali19";
+        return DriverManager.getConnection(url, user, password);
+    }   
+}
