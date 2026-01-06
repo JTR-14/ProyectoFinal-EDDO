3fcc0e271d590b63425b45c135eb48dcd7615dd0
yhair12331 <yahir1234valde@gmail.com>
2026-01-06 16:14:09 -0500

    detalle

--- Supermercado/src/main/java/modelo/DetalleVenta.java
+++ Supermercado/src/main/java/modelo/DetalleVenta.java
@@ -15,5 +15,79 @@ public class DetalleVenta {
     private int cantidad;
     private double precioUnitario;
     private double subtotal;
-    
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(int idVenta, int idProducto, int cantidad, double precioUnitario, double subtotal) {
+        this.idVenta = idVenta;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotal = subtotal;
+    }
+
+    public DetalleVenta(int idDetalle, int idVenta, int idProducto, int cantidad, double precioUnitario, double subtotal) {
+        this.idDetalle = idDetalle;
+        this.idVenta = idVenta;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotal = subtotal;
+    }
+
+    public int getIdDetalle() {
+        return idDetalle;
+    }
+
+    public void setIdDetalle(int idDetalle) {
+        this.idDetalle = idDetalle;
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleVenta{" + "idDetalle=" + idDetalle + ", idVenta=" + idVenta + 
+               ", idProducto=" + idProducto + ", cantidad=" + cantidad + 
+               ", subtotal=" + subtotal + '}';
+    }
 }
